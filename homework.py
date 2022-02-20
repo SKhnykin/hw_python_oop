8f179432bf952c1504f5d4781e219bfd72124f87
Stanislav Khnykin <stas.kh@inbox.ru>
2022-02-20 23:54:13 +0300

    Правка кода после ревизии 3

--- homework.py
+++ homework.py
@@ -48,7 +48,7 @@ class Training:
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         raise NotImplementedError(f'Переопроедлите метод get_spent_calories в'
-                                  f' {type(self).name}')
+                                  f' {type(self).__name__}')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
